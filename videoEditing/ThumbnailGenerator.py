441424e88ca40de215284bf558b3daaa85af8718
Jérémy Navarro <ohmycomput@gmail.com>
2021-01-31 19:48:57 +0100

    Add generator of thumbnail

---
+++ videoEditing/ThumbnailGenerator.py
@@ -0,0 +1,42 @@
+from PIL import Image, ImageDraw, ImageFont
+
+sizeVideo = (1920, 1080)  #FullHD size
+sizeBorder = 100
+folderDestination = "thumbnails"
+usedFont = "fonts/TexgyrecursorBold-V7Rw.otf"  #Monospaced font
+colorText = (255, 255, 255)  # White
+
+
+class Thumbnail:
+    def __init__(self, textInput, colorBackground, name):
+        value = self._getMetric(textInput, usedFont)
+
+        img = Image.new('RGB', sizeVideo, color=tuple(colorBackground))
+
+        f = ImageFont.truetype(usedFont, value[1])
+        d = ImageDraw.Draw(img)
+        d.text(value[0], textInput, font=f, fill=colorText)
+
+        img.save("{}/{}.png".format(folderDestination, name))
+
+    def _getMetric(self, textInput, font):
+        # Compute K to get sizeFont * K = sizeText
+        # Compute the correct font size of the text to fit in the picture
+        # Compute the ancor of the text
+        s1 = 100
+        y1 = ImageFont.truetype(font, s1).getsize(textInput)[0]
+        metric = y1 / s1
+
+        sizeText = sizeVideo[0] - 2 * sizeBorder
+        fontSize = int(sizeText / metric)
+
+        t = ImageFont.truetype(font, fontSize).getsize(textInput)
+
+        xPosition = (sizeVideo[0] - t[0]) / 2
+        yPosition = (sizeVideo[1] - t[1]) / 2
+
+        return ([(xPosition, yPosition), fontSize])
+
+
+
+Thumbnail("Le corbeau et le renard", [255, 0, 0], "1_2")
